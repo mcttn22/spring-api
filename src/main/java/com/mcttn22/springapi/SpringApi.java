e337e6692066ff598ada943e8b873a378cd2c5b2
Max Cotton <maxcotton22@gmail.com>
2024-09-03 17:09:10 +0100

    chore: Setup project structure and pom.xml

---
+++ src/main/java/com/mcttn22/springapi/SpringApi.java
@@ -0,0 +1,14 @@
+package com.mcttn22.springapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApi.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/mcttn22/springapi/SpringApiTest.java
@@ -0,0 +1,14 @@
+package com.mcttn22.springapi;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SpringApiTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
