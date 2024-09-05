bb91d23b4e000c26b47340c82d557238e64c1472
Max Cotton <maxcotton22@gmail.com>
2024-09-05 14:29:01 +0100

    feat: Add basic POST request mapping

--- src/main/java/com/mcttn22/springapi/ApiController.java
+++ src/main/java/com/mcttn22/springapi/ApiController.java
@@ -1,13 +1,18 @@
 package com.mcttn22.springapi;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiController {
 	@GetMapping("/")
 	public String getData() {
-		return "Hello World!";
+		return "Get request";
+	}
+	@PostMapping("/")
+	public String postData() {
+		return "Post request";
 	}
 }
 
