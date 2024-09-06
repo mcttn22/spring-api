a014a1fee367ad176a248ddd7340101ab2c90470
Max Cotton <maxcotton22@gmail.com>
2024-09-06 15:49:46 +0100

    feat: Add Create data support for POST requests

--- src/main/java/com/mcttn22/springapi/ApiController.java
+++ src/main/java/com/mcttn22/springapi/ApiController.java
@@ -3,6 +3,7 @@ package com.mcttn22.springapi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -15,9 +16,13 @@ public class ApiController {
 	public @ResponseBody Iterable<DataTable> getData() {
 		return dataRepository.findAll();
 	}
+
 	@PostMapping("/")
-	public String postData() {
-		return "Post request";
+	public @ResponseBody String postData(@RequestParam String data) {
+		DataTable dataTable = new DataTable();
+		dataTable.setData(data);
+		dataRepository.save(dataTable);
+		return "Saved " + data;
 	}
 }
 
--- src/main/java/com/mcttn22/springapi/DataTable.java
+++ src/main/java/com/mcttn22/springapi/DataTable.java
@@ -10,10 +10,14 @@ public class DataTable {
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Integer id;
-	private String value;
+	private String data;
 	
 	public String getData() {
-		return value;
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
 	}
 }
 
